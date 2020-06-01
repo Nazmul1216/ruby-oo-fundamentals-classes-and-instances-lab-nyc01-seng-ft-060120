f1c360bf62ed60e21280a3c48fa580f133a87171
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-06-01 22:36:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 class Dog 
 end 
 fido=Dog.new 
 snoopy=Dog.new  
 lassie=Dog.new 
+=======
+class Dog
+end
+fido=Dog.new
+snoopy=Dog.new 
+lassie=Dog.new
+class Person
+end
+adele_goldberg=Person.new 
+alan_kay=Person.new 
+>>>>>>> a45b6ba716e999974161ab1146f8c0fc325419fa
--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,8 @@
 class Person
+<<<<<<< HEAD
 end 
+=======
+end
+>>>>>>> a45b6ba716e999974161ab1146f8c0fc325419fa
 adele_goldberg=Person.new 
 alan_kay=Person.new 
